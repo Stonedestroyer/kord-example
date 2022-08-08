117eb5024a504dcb3b57319e66d0dcd36536843e
Stonedestroyer <1307729+Stonedestroyer@users.noreply.github.com>
2022-08-08 22:40:09 +0200

    Files

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,16 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
 }
 
 dependencies {
+    implementation("dev.kord:kord-core:0.8.0-M15")
+    implementation("io.github.cdimascio:dotenv-kotlin:6.3.1")
+    implementation("org.codehaus.groovy:groovy:3.0.12")
+    implementation("org.fusesource.jansi:jansi:2.4.0")
+    implementation("ch.qos.logback:logback-classic:1.2.11")
+    implementation("io.github.microutils:kotlin-logging:2.1.23")
     testImplementation(kotlin("test"))
 }
 
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,41 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import dev.kord.common.entity.Snowflake
+import dev.kord.core.Kord
+import dev.kord.core.builder.kord.KordBuilder
+import dev.kord.core.entity.ReactionEmoji
+import dev.kord.core.event.Event
+import dev.kord.core.event.message.MessageCreateEvent
+import dev.kord.core.event.user.UserUpdateEvent
+import dev.kord.core.on
+import dev.kord.core.supplier.EntitySupplyStrategy
+import dev.kord.gateway.Intents
+import dev.kord.gateway.PrivilegedIntent
+import io.github.cdimascio.dotenv.dotenv
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import mu.KotlinLogging
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+suspend fun main(args: Array<String>) {
     println("Program arguments: ${args.joinToString()}")
+
+    val log = KotlinLogging.logger {  }
+    val dotenv = dotenv()
+    val kordBuilder = KordBuilder(dotenv["TOKEN"])
+    kordBuilder.defaultStrategy = EntitySupplyStrategy.cacheWithRestFallback
+    val kord = kordBuilder.build()
+    val pingPong = ReactionEmoji.Unicode("\uD83C\uDFD3")
+
+    kord.on<MessageCreateEvent> {
+        this.cancel()
+    }
+
+    kord.on<UserUpdateEvent> {
+        this.cancel()
+    }
+
+    kord.on<Event> { this.cancel() }
+
+    kord.login {
+        @OptIn(PrivilegedIntent::class)
+        intents = Intents.all
+    }
 }
